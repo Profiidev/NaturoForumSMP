a9f72e3120c59f295d0be46e29d6c146a5585a46
Profiidev <benjamin@familiebuerger.com>
2023-10-13 13:08:31 +0200

    Added smpupdate Command

---
+++ src/main/java/de/benji/naturoforumsmp/Main/SMP/Commands/PluginUpdateCommand.java
@@ -0,0 +1,33 @@
+package de.benji.naturoforumsmp.Main.SMP.Commands;
+
+import de.benji.naturoforumsmp.API.Strings.Messages.CommandMessages;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class PluginUpdateCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if(!sender.isOp()) {
+            sender.sendMessage(CommandMessages.notAllowed);
+            return true;
+        }
+
+        sender.sendMessage("§eStarting update...");
+        try {
+            Runtime rt = Runtime.getRuntime();
+            rt.exec("cp /home/Minecraft/plugins/NaturoForumSMP-1.19.jar plugins/NaturoForumSMP-1.19.jar");
+        } catch (IOException e) {
+            sender.sendMessage("§cError while updating");
+            return true;
+        }
+        Bukkit.reload();
+        sender.sendMessage("§eUpdate finished.");
+
+        return true;
+    }
+}
